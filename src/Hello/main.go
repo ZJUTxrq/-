c6219b2f5c18877e9a7f192f614d58f06dc9d1f9
Dongzhe <1142824966@qq.com>
2022-05-06 11:06:52 +0800

    linkTest

---
+++ src/Hello/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type Book struct {
+	title string
+	auth  string
+}
+
+//改变不了lisi
+func bookUpdate(book Book) {
+	book.auth = "lisi"
+}
+
+//改变lisi，必须以指针方式
+func bookUp(book *Book) {
+	book.auth = "lisi"
+}
+
+func main() {
+	fmt.Println("hello world")
+
+	var book Book
+	book.title = "Goland"
+	book.auth = "zhangsan"
+
+	fmt.Println(book)
+
+	bookUpdate(book)
+	fmt.Println(book)
+
+	bookUp(&book)
+	fmt.Println(book)
+}
